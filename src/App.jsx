10ac31e8b6ab2790e57a1e3215116e2c6467c6b8
rashiT72 <rashi@t72.club>
2024-04-10 14:39:24 +0530

    header component add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // import React from 'react'
 
+import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar"
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   )
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+import './Header.css'
+import dark_arrow from '../../assets/Images/dark-arrow.png'
+
+const Header = () => {
+    return (
+        <div className='header container'>
+            <div className='header-text'>
+                <h1>We Ensure better education for a better world</h1>
+                <p>Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education</p>
+                <button className='btn'>Explore More <img src={dark_arrow} alt="" /></button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
