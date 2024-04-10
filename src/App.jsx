8dc122721f379b571eadfb2745d912a6a01b3074
rashiT72 <rashi@t72.club>
2024-04-10 15:18:05 +0530

    program component add

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@
 
 import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar"
+import Programs from "./Components/Programs/Programs";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Header />
+
+      <div className="container">
+        <Programs />
+      </div>
+      
     </div>
   )
 }
