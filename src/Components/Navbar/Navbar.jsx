322cf16298cd99f039f86e435b714da66b31a3a3
rashiT72 <rashi@t72.club>
2024-04-15 16:56:25 +0530

    navbar responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import './Navbar.css'
+import { Link } from 'react-scroll';
 import logo from '../../assets/Images/logo.png'
+import menu_icon from '../../assets/Images/menu-icon.png'
 
 const Navbar = () => {
 
@@ -12,17 +14,33 @@ const Navbar = () => {
     })
   }, []);
 
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true)
+  }
+
   return (
     <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
         <img src={logo} alt="" className='logo' />
-        <ul>
-            <li>Home</li>
-            <li>Program</li>
-            <li>About Us</li>
-            <li>Campus</li>
-            <li>Testimonials</li>
-            <li><button className='btn'>Contact Us</button></li>
+        <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
+            <li>
+              <Link to='header' smooth={true} offset={0} duration={500}>Home</Link>
+            </li>
+            <li>
+              <Link to='programs' smooth={true} offset={-260} duration={500}>Program</Link>
+            </li>
+            <li>
+              <Link to='about' smooth={true} offset={-150} duration={500}>About Us</Link>
+            </li>
+            <li>
+              <Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link>
+            </li>
+            <li>
+              <Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link>
+            </li>
+            <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact Us</Link></li>
         </ul>
+        <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu}/>
     </nav>
   )
     
